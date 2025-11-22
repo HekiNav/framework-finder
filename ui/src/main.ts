37f47bcce2c094cf026daf8f0f106f1772fd3d2f
Unto Ahti <developer.hekinav@gmail.com>
2025-11-22 12:31:20 +0200

    finally some script

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,3 +4,32 @@ import './style.css'
 
 import * as bootstrap from 'bootstrap'
 
+document.querySelectorAll(".opens-section").forEach(el => {
+    el.addEventListener("click", () => openSection(el.getAttribute("data-section")!))
+})
+document.querySelectorAll(".range-output").forEach((el) => el.addEventListener("mousemove", () => {
+    document.querySelector(`output[for=${el.id}]`)!.innerHTML = (el as HTMLInputElement).value
+}))
+
+
+
+function openSection(sectionId: string) {
+    const sectionSelector = `#collapse-${sectionId}`
+    const otherSectionsSelector = `.accordion-collapse:not(#collapse-${sectionId})`
+
+    const openedSection = document.querySelector(sectionSelector)!
+
+    openedSection.classList.add("show")
+
+    scrollIntoView(openedSection)
+
+    document.querySelectorAll(otherSectionsSelector).forEach(el => el.classList.remove("show"))
+}
+function scrollIntoView(element: Element) {
+    var innerHeightHalf = (window.innerHeight >> 1);
+
+    var elementRect = element.getBoundingClientRect();
+
+    window.scrollBy((elementRect.left >> 1), elementRect.top - innerHeightHalf);
+}
+
